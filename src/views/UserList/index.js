b582b00c7843af52b919af7a0decd7e0a94fc2ab
mateus gomes <mateusgomesc04@gmail.com>
2023-07-08 14:12:56 -0300

    feat: feat: created UserList component

--- src/views/UserList/index.js
+++ src/views/UserList/index.js
@@ -1,6 +1,61 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Alert, FlatList} from 'react-native';
+import users from '../../mock/users';
+import {Button, Icon, ListItem} from 'react-native-elements';
 
 export default props => {
-  return <Text>UserList</Text>;
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', 'Deseja excluir o usuário', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.warn('delete ' + user.id);
+        },
+      },
+      {
+        text: 'Não',
+      },
+    ]);
+  }
+
+  function getActions(user) {
+    return (
+      <>
+        <Button
+          onPress={() => props.navigation.navigate('UserFor,', user)}
+          type="clear"
+          icon={<Icon mame="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon mame="delete" size={25} color="red" />}
+        />
+      </>
+    );
+  }
+
+  function getUserItem({item: user}) {
+    return (
+      <ListItem
+        leftAvatar={{source: {uri: user.avatarUrl}}}
+        key={user.id}
+        title={user.name}
+        subtitle={user.email}
+        bottomDivider
+        rightElement={getActions(user)}
+        onPress={() => props.navigation.navigate('UserForm')}
+      />
+    );
+  }
+  console.warn(Object.keys(props));
+  return (
+    <View>
+      <FlatList
+        keyExtractor={user => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
 };
